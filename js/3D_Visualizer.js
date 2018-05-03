9970a7ac88c64d5380cfc40c09bd54f7c57d0486
Nate Lao <lao.nathan@yahoo.com>
2018-05-03 13:36:07 -0400

    added a method that draws a path

--- js/3D_Visualizer.js
+++ js/3D_Visualizer.js
@@ -9,18 +9,39 @@ var ctx = canvas.getContext("2d");
 
 ctx.strokeRect(0, 0, side, side);
 
-var m = math.matrix([[0,1],[0,2],[0,3]]);
+var m = math.matrix([[0,0],[50,50],[100,50],[100,100],[50,100],[50,50]]);
 
-draw2DPath(m, null);
+draw2DPath(m, ctx, true);
 
-function draw2DPath(matrix, ctx) {
-    "use strict";
+/* Draws a path based on the given 2D matrix. If transpose parameter is true, the matrix is transposed before processing.*/
+function draw2DPath(matrix, ctx, transpose = false) {
+    //"use strict";
     
-    console.log(matrix.size());
+    if (transpose) {
+        matrix = math.transpose(matrix);
+    }
+    
+    /* Reference methods */
+    console.log(matrix.size()[1]);
     console.log(matrix.get([0,1]));
     matrix.forEach(function (value,index,matrix) {
         console.log(value,' ',index);
     })
+    
+    /* Drawing Algorithm */
+    ctx.beginPath();
+    ctx.moveTo(matrix.get([0,0]),matrix.get([1,0]))
+    var col_index = 1;
+    var col_size = matrix.size()[1];
+    
+    while (col_index < col_size) {
+        ctx.lineTo(matrix.get([0,col_index]),matrix.get([1,col_index]))
+        col_index = col_index + 1;
+    }
+    
+    ctx.stroke();
+    
+    
 }
 
 /* Returns a matrix representation of a cube in the specified coordinates and side length*/
